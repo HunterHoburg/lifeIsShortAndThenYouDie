0fb6ba845292ffc638a50348af5d922fad23cb92
hhoburg <hhoburg@gmail.com>
2016-02-23 13:31:14 -0700

    began knex routing for creating a game

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Life is Short, and Then You Die' });
 });
+
+router.post('create', function(req, res, next) {
+  var newGame = {};
+  knex.insert(newGame).table('games')
+})
 
 module.exports = router;
