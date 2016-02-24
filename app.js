f3061c4367c858fb5eec00d00bb64f9fa2341801
hhoburg <hhoburg@gmail.com>
2016-02-24 09:51:21 -0700

    merging into dev branch

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
+var express = require('express')
+, cors = require('cors')
+, app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', routes);
 app.use('/users', users);
 
