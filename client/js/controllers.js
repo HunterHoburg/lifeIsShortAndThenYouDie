61abb865db86b37c2fe593a18eabecdc3fc247d4
hhoburg <hhoburg@gmail.com>
2016-02-24 12:57:41 -0700

    commented out knexfile for now, and got stories route working

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -12,7 +12,17 @@ angular.module("app")
     vm.title = "Life is Short, and Then You Die :)";
     //QUERYING THE GUARDIAN FOR 50 STORIES
     vm.getArticles = function(topic) {
-      $http.get('http://content.guardianapis.com/search?q='+topic+'&page-size=50&from-date=2015-01-01&section=world&api-key=5297a5ae-063b-42f1-a315-22e0168546e0&show-blocks=all').success(function(res) {
+      var topicArr = topic.split(' ');
+      var parsedTopic;
+      for (var i = 0; i < topicArr.length; i++) {
+        if(topicArr.length > 1) {
+          topicArr[i] += '+';
+          parsedTopic += topicArr[i];
+        } else {
+          parsedTopic = topicArr[i];
+        }
+      }
+      $http.get('http://content.guardianapis.com/search?q='+parsedTopic+'&page-size=50&from-date=2015-01-01&section=world&api-key=5297a5ae-063b-42f1-a315-22e0168546e0&show-blocks=all').success(function(res) {
         console.log(res);
         vm.currentTitle = res.response.results[0].webTitle;
         vm.currentURL = res.response.results[0].webUrl;
@@ -38,42 +48,24 @@ angular.module("app")
             console.log('error: ' + data);
           })
 
-          //Here will go the knex function to push stories into the table
+          //Lo, the knex function to push stories into the table
           var title = res.response.results[i].webTitle;
           var url = res.response.results[i].webUrl;
-
+          var type = res.response.results[i].type;
           $http({
             method: 'POST',
             url: 'http://localhost:3000/insert',
             data: {
               title: title,
               url: url,
-              sentiment: sentiment
+              sentiment: sentiment,
+              game_id: i,
+              type: type
             }
           }).then(function(data) {
             console.log('success ' + data);
-            console.log(sentiment);
-          })
+          });
         }
       })
     };
   };
-
-
-
-  //FUNCTION TO GET SENTIMENT, WILL IMPLEMENT ON STORIES PULLED FROM STORIES TABLE
-  // $http({
-  //   method: 'POST',
-  //   url: 'https://buzzlogix-text-analysis.p.mashape.com/sentiment',
-  //   headers: {
-  //   'X-Mashape-Key': 'tKN2shyLLimshtwjog91r18SHQdVp1NAYE2jsn83if2xCZHEia',
-  //   'content-type': 'text/plain'
-  //   },
-  //   data: {text: res.response.results[1].blocks.body[0].bodyTextSummary}
-  // }).then(function(data) {
-  //   console.log(data.data.Predicted_Class)
-  //   console.log(data.data.Probability);
-  //   vm.currentSentiment = data.data.Predicted_Class;
-  // }, function(data) {
-  //   console.log('error: ' + data);
-  // })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 var knex = require('knex')({
   client: 'pg',
   connection: process.env.DB_HOST
-})
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,21 +15,24 @@ router.get('/', function(req, res, next) {
 router.post('create', function(req, res, next) {
   var newGame = {};
   knex.insert(newGame).table('games')
-})
+});
 
 router.post('/insert', function(req, res, next) {
   var newStory = {
     title: req.body.title,
     url: req.body.url,
-    sentiment: req.body.sentiment
+    game_id: req.body.game_id,
+    type: req.body.type
+    //Remember to add comment above when including sentiment column
+    // sentiment: req.body.sentiment
   };
-  // newStory.title = req.body.title;
-  // newStory.url = req.body.url;
-  // newStory.sentiment = req.body.sentiment;
   console.log(newStory);
-  knex.insert(newStory).into('stories');
+  return knex.insert(newStory).into('stories').then(function(res) {
+    console.log('super success!');
+  });
   // var toLog = knex.select('*').from('stories');
   // console.log(toLog.title);
-})
+  res.send('error: ' + res);
+});
 
 module.exports = router;
